70982719fcab6afde0be59041c4bb3c3d44fb939
Prabin Sigdel <161495459+prabin2021@users.noreply.github.com>
2025-04-24 12:21:49 +0545

    Delete Sipalaya_Backend directory

--- Sipalaya_Backend/stud_portal/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudPortalConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "stud_portal"
-def ready(self):
-    import stud_portal.signals
